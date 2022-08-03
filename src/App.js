a3ceca8f409d55613b093cc75c638d341f1f9c71
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2022-08-03 11:41:15 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { About } from './About'
+import { Contact } from './Contact'
+import Home from './Home'
+import { Navbar } from './Navbar'
+import { NotFound } from './NotFound'
+import { Team } from './Team'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+      <Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/team" element={<Team />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ src/Team.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Team = () => {
+  return (
+    <section>Team</section>
+  )
+}
